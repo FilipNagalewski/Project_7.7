995bfd90adb133a09d07e0c8f27beb61c8d1ca93
Filip Nagalewski <nagalewski@gmail.com>
2017-03-19 01:00:56 +0100

    ifjbnv

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,24 +29,12 @@ module.exports = function(grunt) {
 					spawn: false,
 				},
 			}
-		},
-		browserSync: {
-    		bsFiles: {
-        		src : ['css/*.css']
-    		},
-    		options: {
-    			watchTask: true,
-        		server: {
-            		baseDir: "./"
-        		}
-    		}
 		}
 	});
 	// Load the plugins tasks
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-browser-sync');
 	// Default task (s)
-	grunt.registerTask('default', ['sass', 'imagemin', 'watch', "browserSync"]);
+	grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
